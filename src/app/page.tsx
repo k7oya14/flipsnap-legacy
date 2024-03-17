a4c0c9f810a54bc78a5f5482ccff2735d3586275
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-03-17 19:06:21 +0900

    Add Lock Screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import ReactCardFlip from "react-card-flip";
+import { LockClosedIcon } from "@heroicons/react/24/solid";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const [isFlipped, setIsFlipped] = useState([
@@ -20,30 +22,30 @@ export default function Home() {
 
   const handleClick = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    roomId: number
+    index: number
   ) => {
     e.preventDefault();
     setIsFlipped((prevFlips) => {
       const updatedFlips = [...prevFlips];
-      updatedFlips[roomId - 1] = !updatedFlips[roomId - 1];
+      updatedFlips[index - 1] = !updatedFlips[index - 1];
       return updatedFlips;
     });
   };
 
   return (
     <div className="lg:px-40 p-5 grid lg:grid-cols-3 grid-cols-2 gap-3">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((roomId) => (
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((index) => (
         <ReactCardFlip
-          key={roomId}
-          isFlipped={isFlipped[roomId - 1]}
+          key={index}
+          isFlipped={isFlipped[index - 1]}
           flipDirection="horizontal"
           flipSpeedBackToFront={0.5}
           flipSpeedFrontToBack={0.5}
           infinite={true}
-          cardZIndex={`${roomId / 3}`}
+          cardZIndex={`${index / 3}`}
         >
           <div
-            onClick={(e) => handleClick(e, roomId)}
+            onClick={(e) => handleClick(e, index)}
             className="p-0 hover:cursor-pointer rounded-md"
           >
             <Image
@@ -51,22 +53,31 @@ export default function Home() {
               height={400}
               className="rounded-md"
               alt=""
-              src={`https://source.unsplash.com/collection/1346951/1200x1600?sig=${roomId}`}
+              src={`https://source.unsplash.com/collection/1346951/1200x1600?sig=${index}`}
             />
           </div>
           <div
-            onClick={(e) => handleClick(e, roomId)}
-            className="hover:cursor-pointer rounded-md"
+            onClick={(e) => handleClick(e, index)}
+            className="relative hover:cursor-pointer rounded-lg border-[3px] border-gray-200 "
           >
             <Image
               width={300}
               height={400}
-              className="rounded-md"
+              className="rounded-md blur-lg "
               alt=""
               src={`https://source.unsplash.com/collection/1346951/1200x1600?sig=${
-                roomId + 10
+                index + 10
               }`}
             />
+            <div className="absolute inset-x-0 bottom-0 h-full w-full bg-gradient-to-b from-transparent to-black rounded-b">
+              <div className="absolute  flex flex-col justify-center items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <LockClosedIcon className="h-10 w-10 left-1/2 text-white" />
+                <p className="text-white">Locked</p>
+                <Button className="mt-2 bg-white hover:bg-slate-100 rounded-full text-black font-bold">
+                  Follow
+                </Button>
+              </div>
+            </div>
           </div>
         </ReactCardFlip>
       ))}
