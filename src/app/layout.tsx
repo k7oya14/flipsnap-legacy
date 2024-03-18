6850ff27d58b114cc65a9122d2e67f8e3fc35190
Koya Saka <k7oya14@gmail.com>
2024-03-18 19:11:03 +0900

    Add Header component and signin/out feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
+
+// import { redirect } from 'next/navigation'
+// import { getServerSession } from 'next-auth'
+// import { ReactNode } from 'react'
+// import { authOptions } from '~/lib/auth'
+
+// const AuthLayout = async ({ children }: { children: ReactNode }) => {
+//   const session = await getServerSession(authOptions)
+//   if (!session) redirect('/login')
+
+//   return <>{children}</>
+// }
+
+// export default AuthLayout
