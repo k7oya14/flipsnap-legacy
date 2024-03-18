5a77ff0955ae9f92d2c283e4db79fc86cad46519
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-03-18 20:11:01 +0900

    add avater and icon

--- src/components/ImageFront.tsx
+++ src/components/ImageFront.tsx
@@ -1,5 +1,7 @@
+import { ArrowsPointingOutIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 type Props = {
   col: number;
@@ -14,15 +16,21 @@ const ImageFront = (props: Props) => {
     <div>
       <div
         onClick={(e) => handleClick(col * 4 + row)}
-        className="hover:cursor-pointer rounded-md"
+        className="group relative rounded-md my-2 overflow-hidden"
       >
+        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-30 transition-opacity duration-300"></div>
         <Image
           width={500}
           height={500}
-          className="rounded-md py-2"
+          className="rounded-md"
           alt=""
           src={src}
         />
+        <Avatar className="absolute bottom-2 left-2 invisible group-hover:visible">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <ArrowsPointingOutIcon className="absolute bottom-2 right-2 invisible group-hover:visible h-5 w-5 text-slate-200" />
       </div>
     </div>
   );
