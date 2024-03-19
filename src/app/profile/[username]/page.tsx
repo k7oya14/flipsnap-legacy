970acce49d2b60adf10345aabd54b053b2723af3
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-03-19 20:01:51 +0900

    add profile page without animation

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,12 @@
+import { Profile } from "@/components/profile";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default page;
