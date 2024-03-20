becad5b11d10e1b56ee0185aed20db581dc0b423
Koya Saka <k7oya14@gmail.com>
2024-03-20 20:01:59 +0900

    Add fetchPost API usage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,10 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-// Usage :
-// const data = await fetchLatestPosts(1, null);
-// const cursorPostId = useCursor(data);
-// const data2 = await fetchMoreLatestPosts(1, null,cursorPostId);
 export function useCursor(data: Post[]) {
   return data[data.length - 1].id;
 }
