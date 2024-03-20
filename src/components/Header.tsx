5e421048a71cf31384d30b3d9bf0629956d521c5
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-03-20 17:28:28 +0900

    change font-family and add post icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,33 @@
 import { auth } from "@/lib/auth";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Pacifico, Roboto_Slab } from "next/font/google";
+import { BanknotesIcon } from "@heroicons/react/24/outline";
+
+const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
+const robotoSlab = Roboto_Slab({ weight: "400", subsets: ["latin"] });
 
 const Header = async () => {
-  const session = await auth();
+  const session = await auth(); //sessionがnullログインしてない
+  console.log(session);
   return (
     <header className="">
-      <nav className="flex items-center justify-between p-4">
-        <Link href="/">FlipSnap</Link>
-        <div className="flex space-x-4">
-          <Link href="/posts/create" scroll={false}>
-            Post
+      <nav className="flex items-center justify-between p-2">
+        <div className={`${pacifico.className} flex items-center`}>
+          <Image src="/logo.png" alt="FlipSnap" width={60} height={60} />
+          <Link href="/" className="font-bold text-xl">
+            FlipSnap
+          </Link>
+        </div>
+        <div className={`${robotoSlab.className} flex space-x-4`}>
+          <Link
+            href="/posts/create"
+            scroll={false}
+            className="flex items-center"
+          >
+            <p>Post</p>
+            <BanknotesIcon className="ml-[0.8px] w-6 h-6 text-black" />
           </Link>
           {session ? (
             <Link href="/api/auth/signout">SignOut</Link>
