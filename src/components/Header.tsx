489066c30dcd5ecd137046ace1bd251e865c5c76
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-03-20 17:48:00 +0900

    add Icon and Dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,22 @@ import React from "react";
 import { Pacifico, Roboto_Slab } from "next/font/google";
 import { BanknotesIcon } from "@heroicons/react/24/outline";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
 const robotoSlab = Roboto_Slab({ weight: "400", subsets: ["latin"] });
 
 const Header = async () => {
   const session = await auth(); //sessionがnullログインしてない
-  console.log(session);
+  const user = session?.user;
   return (
     <header className="">
       <nav className="flex items-center justify-between p-2">
@@ -30,7 +40,25 @@ const Header = async () => {
             <BanknotesIcon className="ml-[0.8px] w-6 h-6 text-black" />
           </Link>
           {session ? (
-            <Link href="/api/auth/signout">SignOut</Link>
+            // <Link href="/api/auth/signout">SignOut</Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage src={user?.image!} />
+                  <AvatarFallback>{user?.name!}</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>{user?.name}</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <Link href={`/profile/${user?.id}`}>
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                </Link>
+                <Link href="/api/auth/signout">
+                  <DropdownMenuItem>Sign Out</DropdownMenuItem>
+                </Link>
+              </DropdownMenuContent>
+            </DropdownMenu>
           ) : (
             <Link href="/api/auth/signin">SignIn</Link>
           )}
