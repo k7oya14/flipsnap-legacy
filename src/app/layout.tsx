fb2dda8736f54ac1e8cdf822e122b7b2dca47265
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-03-21 17:57:27 +0900

    change font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { Suspense } from "react";
 import Loading from "@/components/Loading";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Noto_Sans_JP({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "FlipSnap",
