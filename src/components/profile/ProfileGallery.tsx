ee774f82e46c1ce4e0cffd4383f312505b372eef
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-04-06 00:54:49 +0900

    fix profile page error when no posts

--- src/components/profile/ProfileGallery.tsx
+++ src/components/profile/ProfileGallery.tsx
@@ -3,6 +3,7 @@ import { useCursorById } from "@/lib/utils";
 import { fetchUserPostsById } from "@/lib/fetch";
 import ProfileLoadMore from "./ProfileLoadMore";
 import ProfilePost from "./ProfilePost";
+import { Instagram, SwitchCamera } from "lucide-react";
 
 type Props = {
   userInfo: UserInfo;
@@ -15,7 +16,12 @@ export async function ProfileGallery(props: Props) {
   const { cursorById } = useCursorById();
 
   return (
-    <div className="grid sm:grid-cols-3 grid-cols-2 px-1 sm:px-2 gap-1 sm:gap-2 sm:mt-4">
+    <div
+      className={`${
+        firstPosts.length === 0 ||
+        "grid sm:grid-cols-3 grid-cols-2 px-1 sm:px-2 gap-1 sm:gap-2 sm:mt-4"
+      }`}
+    >
       {firstPosts.map((post, index) => (
         <ProfilePost
           key={post.id}
@@ -25,11 +31,20 @@ export async function ProfileGallery(props: Props) {
           userInfo={userInfo}
         />
       ))}
-      <ProfileLoadMore
-        myId={myId}
-        userInfo={userInfo}
-        cursorId={cursorById(firstPosts)}
-      />
+      {firstPosts.length === 0 ? (
+        <div className="pt-6 pb-10 flex flex-col sm:flex-row items-center justify-center">
+          <Instagram className="size-16 lg:size-20 mb-2 sm:mr-4" />
+          <p className="text-2xl sm:text-3xl lg:text-4xl font-semibold">
+            No posts yet
+          </p>
+        </div>
+      ) : (
+        <ProfileLoadMore
+          myId={myId}
+          userInfo={userInfo}
+          cursorId={cursorById(firstPosts)}
+        />
+      )}
     </div>
   );
 }
