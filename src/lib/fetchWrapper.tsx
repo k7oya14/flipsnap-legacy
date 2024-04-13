bd473e1fd395cf19ce4f74c0ba5ecc1f0ae006b5
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-04-13 23:37:10 +0900

    comment out HomePost

--- src/lib/fetchWrapper.tsx
+++ src/lib/fetchWrapper.tsx
@@ -3,7 +3,7 @@
 import { unstable_noStore as noStore } from "next/cache";
 import prisma from "./prismaClient";
 import { SpHomePost } from "@/components/smartphone/SpHomePost";
-import HomePost from "@/components/home/HomePost";
+// import HomePost from "@/components/home/HomePost";
 
 // export async function fetchLatestPostsComponent(
 //   take: number,
@@ -138,69 +138,69 @@ import HomePost from "@/components/home/HomePost";
 //   }
 // }
 
-export async function fetchLatestPostsSpComponent(
-  take: number,
-  myId: string | undefined | null
-) {
-  noStore();
-  try {
-    const data = await prisma.post.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-      include: {
-        author: {
-          select: {
-            username: true,
-            image: true,
-            name: true,
-          },
-        },
-      },
-      take,
-    });
-    if (myId) {
-      const posts = await Promise.all(
-        data.map(async (post) => {
-          // const relationship = await fetchUserRelationship(myId, post.authorId);
-          return {
-            ...post,
-            author: {
-              ...post.author,
-              // relationship,
-            },
-          };
-        })
-      );
-      return {
-        component: posts.map((post) => (
-          <SpHomePost key={post.id} post={post} myId={myId} />
-        )),
-        cursorId: posts[posts.length - 1].id,
-      };
-    } else {
-      const posts = await Promise.all(
-        data.map(async (post) => {
-          return {
-            ...post,
-            author: {
-              ...post.author,
-              // relationship: UserRelationship.NoSession,
-            },
-          };
-        })
-      );
-      return {
-        component: posts.map((post) => (
-          <SpHomePost key={post.id} post={post} myId={myId} />
-        )),
-        cursorId: posts[posts.length - 1].id,
-      };
-    }
-  } catch (error) {
-    throw new Error("Failed to fetch first latest posts.");
-  }
-}
+// export async function fetchLatestPostsSpComponent(
+//   take: number,
+//   myId: string | undefined | null
+// ) {
+//   noStore();
+//   try {
+//     const data = await prisma.post.findMany({
+//       orderBy: {
+//         createdAt: "desc",
+//       },
+//       include: {
+//         author: {
+//           select: {
+//             username: true,
+//             image: true,
+//             name: true,
+//           },
+//         },
+//       },
+//       take,
+//     });
+//     if (myId) {
+//       const posts = await Promise.all(
+//         data.map(async (post) => {
+//           // const relationship = await fetchUserRelationship(myId, post.authorId);
+//           return {
+//             ...post,
+//             author: {
+//               ...post.author,
+//               // relationship,
+//             },
+//           };
+//         })
+//       );
+//       return {
+//         component: posts.map((post) => (
+//           <SpHomePost key={post.id} post={post} myId={myId} />
+//         )),
+//         cursorId: posts[posts.length - 1].id,
+//       };
+//     } else {
+//       const posts = await Promise.all(
+//         data.map(async (post) => {
+//           return {
+//             ...post,
+//             author: {
+//               ...post.author,
+//               // relationship: UserRelationship.NoSession,
+//             },
+//           };
+//         })
+//       );
+//       return {
+//         component: posts.map((post) => (
+//           <SpHomePost key={post.id} post={post} myId={myId} />
+//         )),
+//         cursorId: posts[posts.length - 1].id,
+//       };
+//     }
+//   } catch (error) {
+//     throw new Error("Failed to fetch first latest posts.");
+//   }
+// }
 
 export async function fetchMoreLatestPostsSpComponent(
   take: number,
