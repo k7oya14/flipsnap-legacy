db7589777b69154f523c4c3e6df27547e2ae3303
Koya Saka <k7oya14@gmail.com>
2024-04-19 15:48:28 +0900

    change db schema to support comments feature

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,6 +15,17 @@ type User = {
   created_at: Date;
 };
 
+type Post =  {
+  id: string;
+  username: string | null;
+  email: string | null;
+  emailVerified: Date | null;
+  image: string | null;
+  name: string | null;
+  bio: string;
+  created_at: Date;
+}
+
 const userCount = 64; // TODO : Change this parameter
 
 async function createUsers() {
@@ -69,13 +80,39 @@ async function createFollowRelations(users: User[]) {
 
 const postUserCount = 20; // TODO : Change this parameter
 
-async function createPosts(users: User[]) {
+async function createPost(users: User[]) {
+  const posts = [];
   let imgIndex = 0;
   for (const user of users.slice(0, postUserCount)) {
-    await prisma.user.update({
+    const post = await prisma.user.update({
       where: { id: user.id },
       data: {
         posts: {
+          create: {
+            imgFront: imgSets[imgIndex % imgSets.length],
+            imgBack: imgSets[imgIndex % imgSets.length],
+            caption: faker.lorem.lines(),
+            createdAt: faker.date.past(),
+          },
+        },
+      },
+    });
+    imgIndex++;
+    posts.push(post);
+  }
+  return posts;
+}
+
+async function createComments(users : User[], posts: Post[]) {
+  for (const user of users) {
+    const commentPostCount = faker.number.int({ min: 0, max: postUserCount });
+    const commentPosts = faker.helpers
+      .arrayElements(posts, commentPostCount)
+
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        Comment: {
           create: {
             imgFront: imgSets[imgIndex % imgSets.length],
             imgBack: imgSets[imgIndex % imgSets.length],
@@ -109,7 +146,7 @@ async function createPosts(users: User[]) {
 async function main() {
   const users = await createUsers();
   await createFollowRelations(users);
-  await createPosts(users);
+  const posts = await createPost(users);
   // await hishiwatPosts();
 }
 
