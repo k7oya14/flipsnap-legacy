730bfdc33d478a2c7344ceccb615e982a2c45880
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-04-19 16:18:17 +0900

    delete card of profile page

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,24 +1,21 @@
 import Profile from "@/components/profile/Profile";
 import ProfileGallerySkeleton from "@/components/skeleton/ProfileGallerySkeleton";
 import ProfileInformationSkeleton from "@/components/skeleton/ProfileInformationSkeleton";
-import { Card } from "@/components/ui/card";
 import React, { Suspense } from "react";
 
 const Page = async ({ params }: { params: { username: string } }) => {
   return (
     <div className="my-1 sm:my-2 max-w-5xl mx-auto">
-      <Card className="min-h-screen">
-        <Suspense
-          fallback={
-            <>
-              <ProfileInformationSkeleton />
-              <ProfileGallerySkeleton />
-            </>
-          }
-        >
-          <Profile username={params.username} />
-        </Suspense>
-      </Card>
+      <Suspense
+        fallback={
+          <>
+            <ProfileInformationSkeleton />
+            <ProfileGallerySkeleton />
+          </>
+        }
+      >
+        <Profile username={params.username} />
+      </Suspense>
     </div>
   );
 };
