1e66a2b60886c316cfa4f9704517621487fae8f4
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-04-19 19:15:22 +0900

    change backgroundColor card

--- src/app/profile/[username]/follower/page.tsx
+++ src/app/profile/[username]/follower/page.tsx
@@ -7,7 +7,7 @@ const Follower = async ({ params }: { params: { username: string } }) => {
   return (
     <>
       <h1 className="text-2xl font-semibold text-center mt-4">Follower</h1>
-      <Card className="m-4 max-w-[95vw] w-[500px] mx-auto shadow-lg bg-neutral-100">
+      <Card className="m-4 max-w-[95vw] w-[500px] mx-auto shadow-lg">
         <CardContent className="py-1">
           <Suspense fallback={<FollowSkeleton />}>
             <FollowerList username={params.username} />
