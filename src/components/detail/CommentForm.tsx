3aa61fda81374bb685c173b8274e023590dc67fb
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-04-23 20:28:04 +0900

    add pending

--- src/components/detail/CommentForm.tsx
+++ src/components/detail/CommentForm.tsx
@@ -5,6 +5,7 @@ import { AutosizeTextarea } from "../ui/autosizeTextarea";
 import { createComment } from "@/lib/actions";
 import { useFormState, useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
+import CommentPostButton from "./CommentPostButton";
 
 type Props = {
   myId: string | undefined | null;
@@ -19,17 +20,16 @@ const CommentForm = (props: Props) => {
   };
   const createCommentWithId = createComment.bind(null, myId!, postId);
   const [state, dispatch] = useFormState(createCommentWithId, initialState);
-  const { pending } = useFormStatus();
   const formRef = useRef<HTMLFormElement>(null);
 
   useEffect(() => {
-    if (pending || state.message === "Comment created successfully.") {
+    if (state.message === "Comment created successfully.") {
       formRef.current?.reset();
     }
-  }, [pending, state.message]);
+  }, [state.message]);
 
   return (
-    <form action={dispatch} ref={formRef}>
+    <form action={dispatch} ref={formRef} className="flex items-center">
       <AutosizeTextarea
         name="content"
         id="content"
@@ -39,7 +39,7 @@ const CommentForm = (props: Props) => {
         className="border-t h-10 dialog-scroll border-t-gray-200 bg-neutral-100 resize-none rounded-none rounded-br-lg focus-visible:ring-offset-0 focus-visible:ring-0 w-full"
         placeholder="Add a comment..."
       />
-      <Button type="submit">Post</Button>
+      <CommentPostButton />
     </form>
   );
 };
