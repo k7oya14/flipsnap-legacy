66e9be5cfbb4c3d50c95ebd07ff42852235fe929
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-04-26 16:37:47 +0900

    add Head metadata

--- src/components/detail/CommentTextareaAndButton.tsx
+++ src/components/detail/CommentTextareaAndButton.tsx
@@ -3,6 +3,7 @@ import { Button } from "../ui/button";
 import { useFormStatus } from "react-dom";
 import { LoaderCircle } from "lucide-react";
 import { AutosizeTextarea } from "../ui/autosizeTextarea";
+import Head from "next/head";
 
 type Props = {
   commentContent: string;
@@ -20,23 +21,24 @@ const CommentTextareaAndButton = (props: Props) => {
   }, [pending]);
   return (
     <>
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
-      >
-        <AutosizeTextarea
-          commentContent={commentContent}
-          name="content"
-          id="content"
-          value={commentContent}
-          onChange={(e) => setCommentContent(e.target.value)}
-          aria-describedby="content-error"
-          maxHeight={100}
-          minHeight={37}
-          className={`h-10 dialog-scroll border-0 bg-neutral-100 resize-none rounded-none focus-visible:ring-offset-0 focus-visible:ring-0 w-full`}
-          placeholder={pending ? "Processing..." : "Add a comment..."}
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
         />
-      </meta>
+      </Head>
+      <AutosizeTextarea
+        commentContent={commentContent}
+        name="content"
+        id="content"
+        value={commentContent}
+        onChange={(e) => setCommentContent(e.target.value)}
+        aria-describedby="content-error"
+        maxHeight={100}
+        minHeight={37}
+        className={`h-10 dialog-scroll border-0 bg-neutral-100 resize-none rounded-none focus-visible:ring-offset-0 focus-visible:ring-0 w-full`}
+        placeholder={pending ? "Processing..." : "Add a comment..."}
+      />
       <Button
         type="submit"
         className={`rounded-none sm:rounded-br-lg bg-neutral-100 hover:bg-neutral-100 ${
