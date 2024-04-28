4924585db5808a8d31019611e3dc17bcf54479b2
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-04-28 21:50:48 +0900

    fix an error when repeatedly hitting the follow button

--- src/components/profile/FollowStatusButton.tsx
+++ src/components/profile/FollowStatusButton.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import React, { useOptimistic } from "react";
+import React, { useRef, useState } from "react";
 import { Button } from "../ui/button";
 import { Follow, UnFollow } from "@/lib/actions";
 import { UserRelationship } from "@/lib/definitions";
+import { MotionDiv } from "../MotionDiv";
+import { useFormStatus } from "react-dom";
+import { useDebouncedCallback } from "use-debounce";
 
 type Props = {
   myId: string;
@@ -14,54 +17,95 @@ type Props = {
 const FollowStatusButton = (props: Props) => {
   const { myId, userId, relationship } = props;
 
-  const [optimisticRelationship, updateOptimisticRelationship] = useOptimistic<
-    UserRelationship,
-    UserRelationship
-  >(relationship, (state, newState: UserRelationship) => newState);
+  const [optimisticRelationship, updateOptimisticRelationship] =
+    useState(relationship);
+  const realityRelationship = useRef(relationship);
+  const { pending } = useFormStatus();
+
+  const onDebounceFollow = useDebouncedCallback(async () => {
+    if (pending) return;
+
+    if (realityRelationship.current === optimisticRelationship) return;
+
+    if (
+      optimisticRelationship === UserRelationship.Following ||
+      optimisticRelationship === UserRelationship.Mutual
+    ) {
+      try {
+        await Follow(myId, userId);
+        realityRelationship.current++;
+        updateOptimisticRelationship(realityRelationship.current);
+      } catch (error) {
+        updateOptimisticRelationship(realityRelationship.current);
+      }
+    } else {
+      try {
+        await UnFollow(myId, userId);
+        realityRelationship.current--;
+        updateOptimisticRelationship(realityRelationship.current);
+      } catch (error) {
+        updateOptimisticRelationship(realityRelationship.current);
+      }
+    }
+  }, 500);
+
+  const handleOnClick = () => {
+    if (
+      optimisticRelationship === UserRelationship.Follower ||
+      optimisticRelationship === UserRelationship.None
+    )
+      updateOptimisticRelationship((prev) => prev + 1);
+    else updateOptimisticRelationship((prev) => prev - 1);
+    onDebounceFollow();
+  };
 
   switch (optimisticRelationship) {
     case UserRelationship.Follower:
     case UserRelationship.None:
       return (
-        <div>
-          <form
-            action={async () => {
-              updateOptimisticRelationship(relationship + 1);
-              await Follow(myId, userId);
+        <>
+          <MotionDiv
+            whileTap={{
+              scale: 0.9,
             }}
-            className="flex flex-col"
           >
-            <Button type="submit" className="rounded-full bg-neutral-900">
-              Follow
-            </Button>
-          </form>
+            <form action={handleOnClick} className="flex flex-col">
+              <Button
+                type="submit"
+                className="rounded-full bg-neutral-900 w-24"
+              >
+                Follow
+              </Button>
+            </form>
+          </MotionDiv>
           {optimisticRelationship === UserRelationship.Follower && (
             <p className="text-sm font-normal">You are being followed</p>
           )}
-        </div>
+        </>
       );
     case UserRelationship.Following:
     case UserRelationship.Mutual:
       return (
-        <div>
-          <form
-            action={async () => {
-              updateOptimisticRelationship(relationship - 1);
-              await UnFollow(myId, userId);
+        <>
+          <MotionDiv
+            whileTap={{
+              scale: 0.9,
             }}
-            className="flex flex-col"
           >
-            <Button
-              type="submit"
-              className="rounded-full bg-neutral-300 hover:bg-neutral-400 opacity-80 text-black"
-            >
-              Unfollow
-            </Button>
-          </form>
+            <form action={handleOnClick} className="flex flex-col">
+              <Button
+                type="submit"
+                className="group w-24 rounded-full bg-neutral-300 hover:bg-neutral-400 opacity-80 text-black"
+              >
+                <span className="hidden group-hover:block">Unfollow</span>
+                <span className="block group-hover:hidden">Following</span>
+              </Button>
+            </form>
+          </MotionDiv>
           {optimisticRelationship === UserRelationship.Mutual && (
             <p className="text-sm font-normal">You are being followed</p>
           )}
-        </div>
+        </>
       );
     default:
       break;
