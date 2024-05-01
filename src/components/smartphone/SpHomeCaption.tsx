81f5efa906593f7112e820de5d1bf8e3f6dbab5b
Hikaru Ishiwata <hikaru160830@gmail.com>
2024-05-01 21:18:14 +0900

    prevent extra rendering on SpHomeCaption

--- src/components/smartphone/SpHomeCaption.tsx
+++ src/components/smartphone/SpHomeCaption.tsx
@@ -4,10 +4,13 @@ import React, { useState } from "react";
 
 const SpHomeCaption = ({ caption }: { caption: string }) => {
   const [fullCaption, setFullCaption] = useState(false);
+  const onClickCaption = () => {
+    if (!fullCaption) setFullCaption(true);
+  };
   return (
     <p
       className={`${fullCaption ? "" : "truncate"} hover:cursor-pointer`}
-      onClick={() => setFullCaption(true)}
+      onClick={onClickCaption}
     >
       {caption}
     </p>
