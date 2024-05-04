f678206b609ae6cbd4f2d2ec0804747dfd4ccba4
Koya Saka <k7oya14@gmail.com>
2024-05-05 00:49:00 +0900

    Revert "add PrismaAccelerate"
    This reverts commit 42df1bc6620fbca00fb129ce8f180e30fbc4356d.

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prismaClientSingleton = () => {
-  // return new PrismaClient();
-  return new PrismaClient().$extends(withAccelerate())
+  return new PrismaClient();
   // return new PrismaClient({log: ["query"]},);
 };
 
